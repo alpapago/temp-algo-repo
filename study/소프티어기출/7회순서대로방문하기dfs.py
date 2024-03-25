51782198383185e207083e658a096cc62896d2c1
LeeShinAe <gosin77@snu.ac.kr>
2024-03-25 16:11:30 +0900

    0325소프티어

--- study/소프티어기출/7회순서대로방문하기dfs.py
+++ study/소프티어기출/7회순서대로방문하기dfs.py
@@ -2,15 +2,48 @@ import sys
 input = sys.stdin.readline
 
 n,m = map(int,input().split())
-arr = []
+
+pan = []
 
 for _ in range(n):
     tmp = list(map(int,input().split()))
-    arr.append(tmp)
+    pan.append(tmp)
 
 jum = list()
 for _ in range(m):
     x,y = map(int,input().split())
-    jum.append((x,y))
+    jum.append((x-1,y-1))
 
-def dfs(n):
+ans = 0
+
+def dfs(points, depth):
+    global ans
+
+    if depth == m:
+        ans += 1
+        return 
+
+    r = points[-1][0]
+    c = points[-1][1]
+    # print(points,'경로',ans,'ans')
+
+    for dr, dc in [(-1,0),(1,0),(0,-1),(0,1)]:
+        nr = dr + r
+        nc = dc + c
+        if 0 > nr or 0 > nc or n <= nr or n <= nc:
+            continue
+        if pan[nr][nc] == 1:
+            continue
+        if (nr,nc) not in points:
+            if nr == jum[depth][0] and nc == jum[depth][1]:
+                # print(depth,'depth_1')
+                dfs(points +[(nr,nc)], depth + 1)
+            else:
+                # print(depth,'depth_2')
+                dfs(points+[(nr,nc)], depth)
+
+check = []
+check.append((jum[0][0],jum[0][1]))
+dfs(check,1)
+
+print(ans)    
